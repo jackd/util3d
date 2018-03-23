4225076631dcbe78ffca183ea1cafb1cc1bc82c1
Dominic Jack <thedomjack@gmail.com>
2018-03-23 19:59:30 +1000

    tweaks

--- mayavi_vis.py
+++ mayavi_vis.py
@@ -21,6 +21,26 @@ def vis_point_cloud(points, axis_order='xyz', value=None, **kwargs):
     mlab.points3d(*data, **kwargs)
 
 
+def vis_segmented_cloud(points, idxs, colors=None, **kwargs):
+    if colors is None:
+        colors = (
+            (0, 0, 0),
+            (0, 0, 1),
+            (0, 1, 0),
+            (0, 1, 1),
+            (1, 0, 0),
+            (1, 0, 1),
+            (1, 1, 0),
+            (1, 1, 1),
+        )
+    n_means = np.max(idxs)+1
+    clouds = [points[idxs == i] for i in range(n_means)]
+    for i in range(n_means):
+        cloud = clouds[i]
+        color = colors[i % len(colors)]
+        vis_point_cloud(cloud, color=color, **kwargs)
+
+
 def vis_voxels(voxels, axis_order='xzy', **kwargs):
     data = permute_xyz(*np.where(voxels), order=axis_order)
     if len(data[0]) == 0:
--- point_cloud.py
+++ point_cloud.py
@@ -1,9 +1,10 @@
 import numpy as np
 
 
-def sample_points(x, n_samples, axis=0):
-    """Sample `n_samples` points from cloud `x` with replacement."""
+def sample_points(x, n_samples, axis=0, replace=True):
+    """Sample `n_samples` points from cloud `x` with or without replacement."""
     n_original = x.shape[axis]
-    indices = np.random.uniform(
-        0, n_original, size=(n_samples)).astype(np.int32)
+    indices = np.random.choice(n_original, n_samples, replace=replace)
+    # indices = np.random.uniform(
+    #     0, n_original, size=(n_samples)).astype(np.int32)
     return x.take(indices, axis=axis)
